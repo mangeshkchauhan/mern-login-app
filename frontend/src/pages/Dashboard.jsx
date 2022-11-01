c8aec6bf589547d07d4fd6afc686d7e090499439
Mangesh <63477318+mangeshkchauhan@users.noreply.github.com>
2022-11-02 02:43:05 +0530

    fix

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -22,9 +22,9 @@ function Dashboard() {
 
     if (!user) {
       navigate('/login')
+    }else{
+      dispatch(getGoals())
     }
-
-    dispatch(getGoals())
 
     return () => {
       dispatch(reset())
